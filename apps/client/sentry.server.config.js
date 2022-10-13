7f084c41529f14b7f638dfbfe3b5b6b0ddba3fa6
David's Devel <djgm1206@gmail.com>
2022-10-13 17:44:07 -0400

    ci: fixed error getting sentry DSN

--- apps/client/sentry.server.config.js
+++ apps/client/sentry.server.config.js
@@ -1,11 +1,10 @@
 import * as Sentry from '@sentry/nextjs';
-import env from './config/env';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 const isDev = process.env.NODE_ENV !== 'production';
 
 Sentry.init({
-  dsn: SENTRY_DSN || env.SENTRY_DSN,
+  dsn: SENTRY_DSN,
   tracesSampleRate: 0.25,
   enabled: !isDev,
   debug: false,
