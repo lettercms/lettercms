2e145bf366daaf30744a05ec06c6de268544f816
David's Devel <djgm1206@gmail.com>
2022-10-20 01:16:10 -0400

    chore: Updated file paths

--- apps/cdn/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {join} = require('path');
-
-const app = express();
-
-app.use(express.static(join(__dirname, 'public')));
-
-app.listen(3003, () => console.log('> CDN Listen on port 3003'));
