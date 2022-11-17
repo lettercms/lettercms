b0bd8627338fc2d0ac1b7cb0975a8e814b4c241f
David's Devel <djgm1206@gmail.com>
2022-11-17 01:12:26 -0400

    chore: Improve development script

--- scripts/run-dev.js
+++ scripts/run-dev.js
@@ -1,10 +1,22 @@
+
+const {argv} = require('yargs');
+const {workspaces} = argv;
+
+process.env.DEBUG =  workspaces + ',' + (process.env.DEBUG ||  '');
+
 const { spawn, execSync } = require('child_process');
 const { appendFile } = require('fs/promises');
 const { existsSync, mkdirSync } = require('fs');
 const { join } = require('path');
+const debug = require('debug');
+
+const workspaceArray = workspaces.split(','); 
 
 const base = process.cwd();
 const logPath = join(base, '.logs');
+
+//TODO: add shared env vars on development
+//require('dotenv').config({ path: join(base, '.env')});
 
 const logger = async (workspace, text) => {
   const date = new Date();
@@ -16,39 +28,58 @@ const logger = async (workspace, text) => {
 
   await appendFile(path, log);
   
-  console.log(`${workspace} >`, text.toString());
+  debug(workspace)(text.toString().replace(/\r?\n?$/, ''));
 }
 
 (async function() {
   if (!existsSync(logPath))
     mkdirSync(logPath);
 
-  const api = spawn(join(base, 'node_modules', '.bin', 'next.cmd'), ['dev', join(base, 'apps', 'api'), '--port', '3009']);
-  const dashboard = spawn(join(base, 'node_modules', '.bin', 'next.cmd'), ['dev', join(base, 'apps', 'dashboard'), '--port', '3000']);
   const cdn = spawn('node', [join(base, 'scripts', 'serve-files')]);
 
-  api.stdout.on('data', (data) => {
-    logger('api', data);
-  });
+  if (workspaceArray.includes('api')) {
+    const api = spawn(join(base, 'node_modules', '.bin', 'next.cmd'), ['dev', join(base, 'apps', 'api'), '--port', '3009']);
+    api.stdout.on('data', (data) => {
+      logger('api', data);
+    });
 
-  api.stderr.on('data', (data) => {
-    logger('api', data);
-  });
+    api.stderr.on('data', (data) => {
+      logger('api', data);
+    });
 
-  api.on('close', (code) => {
-    logger('api', `child process exited with code ${code}`);
-  });
+    api.on('close', (code) => {
+      logger('api', `child process exited with code ${code}`);
+    });
+  }
   
-  dashboard.stdout.on('data', (data) => {
-    logger('dashboard', data);
-  });
+  if (workspaceArray.includes('dashboard')) {
+    const dashboard = spawn(join(base, 'node_modules', '.bin', 'next.cmd'), ['dev', join(base, 'apps', 'dashboard'), '--port', '3000']);
+    dashboard.stdout.on('data', (data) => {
+      logger('dashboard', data);
+    });
 
-  dashboard.stderr.on('data', (data) => {
-    logger('dashboard', data);
-  });
+    dashboard.stderr.on('data', (data) => {
+      logger('dashboard', data);
+    });
 
-  dashboard.on('close', (code) => {
-    logger('dashboard', `child process exited with code ${code}`);
-  });
+    dashboard.on('close', (code) => {
+      logger('dashboard', `child process exited with code ${code}`);
+    });
+  }
+
+  if (workspaceArray.includes('client')) {
+    const client = spawn(join(base, 'node_modules', '.bin', 'next.cmd'), ['dev', join(base, 'apps', 'client'), '--port', '3002']);
+    client.stdout.on('data', (data) => {
+      logger('client', data);
+    });
+
+    client.stderr.on('data', (data) => {
+      logger('client', data);
+    });
+
+    client.on('close', (code) => {
+      logger('client', `child process exited with code ${code}`);
+    });
+  }
 
 })()
