7f64e60019ba5544b1cd3faeb5a7afe19fd52747
David's Devel <djgm1206@gmail.com>
2022-12-27 16:28:50 -0400

    linted

--- apps/dashboard/src/components/post/index.jsx
+++ apps/dashboard/src/components/post/index.jsx
@@ -69,27 +69,26 @@ const BlogPost = ({isAdmin, isPreview, user, recommendation: {recommended, simil
         <Base rows={1} title={hasRecommendation ? 'Recomendados para ti' : ''} style={{flexWrap: 'wrap', height: 'auto', width: '100%', borderRadius: '1rem 1rem 0 0', margin: '1rem 0 0'}}>
           {
             hasRecommendation &&
-            <Card
-              title={recommended.title}
-              content={recommended.description}
-              url={recommended.url}
-              fullUrl={recommended.fullUrl}
-              thumbnail={recommended.thumbnail}
-              comments={0}
-              ID={recommended._id}
-            />
-          } 
-          {
-            hasRecommendation &&
-            <Card
-              title={similar.title}
-              content={similar.description}
-              url={similar.url}
-              fullUrl={similar.fullUrl}
-              thumbnail={similar.thumbnail}
-              comments={0}
-              ID={similar._id}
-            />
+            <>
+              <Card
+                title={recommended.title}
+                content={recommended.description}
+                url={recommended.url}
+                fullUrl={recommended.fullUrl}
+                thumbnail={recommended.thumbnail}
+                comments={0}
+                ID={recommended._id}
+              />
+              <Card
+                title={similar.title}
+                content={similar.description}
+                url={similar.url}
+                fullUrl={similar.fullUrl}
+                thumbnail={similar.thumbnail}
+                comments={0}
+                ID={similar._id}
+              />
+            </>
           }
           {/*<Comments id={_id} user={stateUser}/>*/}
         </Base>
