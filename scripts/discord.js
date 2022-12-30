5e5a7151021789ebdea553e8a4c5104809aeb84f
David's Devel <djgm1206@gmail.com>
2022-12-30 12:52:23 -0400

    ci: Added pre deploy notification

--- scripts/discord.js
+++ scripts/discord.js
@@ -5,7 +5,49 @@ const env = process.argv[3];
 
 const isFail = status === 'failed';
 
+
+
 (async function() {
+  let colorCode = '5f4dee';
+  let description = '';
+
+  let fields = [];
+
+  if (status === 'fail' || status === 'success') {
+    fields = [
+      { 
+        name: "Job Number", 
+        value: process.env.CIRCLE_BUILD_NUM
+      }, 
+      {
+        name: "Job URL", 
+        value: `[Visit Job](${process.env.CIRCLE_BUILD_URL})`
+      },
+      {
+        name: 'URL',
+        value: `[View Deployment](https://lettercms-${env}-${process.env.CIRCLE_BRANCH}.vercel.app)`
+      }
+    ];
+  }
+
+  if (status === 'fail') {
+    description = `Ooops! The **${env}** app on **${process.env.CIRCLE_BRANCH}** branch wasn't deployed!`;
+    colorCode = 15555676;
+  } else if (status === 'success') {
+    description = `Successfully deployed **${env}** app on **${process.env.CIRCLE_BRANCH}** branch!`;
+    colorCode = 3394662;
+  } else if (status === 'info') {
+    description = `Starting Deployment of **${process.env.CIRCLE_BRANCH}** branch by [@${process.env.CIRCLE_USERNAME}](https://github.com/${process.env.CIRCLE_USERNAME})`;
+    colorCode = 'f7f7f7';
+
+    fields = [
+      {
+        name: 'Pull Request',
+        value: `[View Changes](${process.env.CI_PULL_REQUEST})`
+      }
+    ];
+  }
+
   const r = await fetch(process.env.DISCORD_DEPLOYMENT_WEBHOOK, {
     method: 'POST',
     headers: {
@@ -14,27 +56,9 @@ const isFail = status === 'failed';
     body: JSON.stringify({
       embeds: [
         { 
-          //"description": , 
-          description: isFail
-            ? `Ooops! The **${env}** app on **${process.env.CIRCLE_BRANCH}** branch wasn't deployed!`
-            : `Successfully deployed **${env}** app on **${process.env.CIRCLE_BRANCH}** branch!`,
-          color: isFail
-            ? 15555676
-            : 3394662, 
-          fields: [
-            { 
-              name: "Job Number", 
-              value: process.env.CIRCLE_BUILD_NUM
-            }, 
-            { 
-              name: "Job URL", 
-              value: `[Visit Job](${process.env.CIRCLE_BUILD_URL})`
-            },
-            {
-              name: 'URL',
-              value: `[View Deployment](https://lettercms-${env}-${process.env.CIRCLE_BRANCH}.vercel.app)`
-            }
-          ]
+          description,
+          color: colorCode,
+          fields
         } 
       ] 
     })
